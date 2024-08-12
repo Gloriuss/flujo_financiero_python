27d9eba0ed5b19ac360430ccbe1e444f4bf5d41c
Tu Nombre <diegogloria.s@gmail.com>
2024-08-12 19:11:12 +0200

    new file:   configuraciones.py 	new file:   costos.py 	new file:   ingresos_y_ventas.py 	modified:   main.py 	new file:   visualizacion.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-from flujo import 
+from configuraciones import configuracion_inicial
+from ingresos_y_ventas import simular_escenarios_ventas, calcular_ingresos
+from visualizacion import visualizar_escenario_unico, visualizar_histograma_ventas, visualizar_distribucion_unidades
+
+# Configuración inicial
+periodo_inicial = configuracion_inicial()
+
+#Simulación de ventas
+ventas_escenarios = simular_escenarios_ventas()
+
+#Cálculo de ingresos
+
+ingresos_agrupados = calcular_ingresos(ventas_escenarios)
+
+#Visualización de resultados
+
+visualizar_escenario_unico(ingresos_agrupados)
+visualizar_histograma_ventas(ventas_escenarios, 3, 34)
+visualizar_distribucion_unidades(ventas_escenarios, 3, 34)
